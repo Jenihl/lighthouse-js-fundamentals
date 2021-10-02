ecfef9e58e0f8496e8a247f5577ace33cf2b6f04
Jen Ihl <jenniferlindatruong@gmail.com>
2021-10-02 21:35:06 +0000

    isodd PROUD OF YOU

---
+++ odd.js
@@ -0,0 +1,8 @@
+//Inside the file, define a function called isOdd.
+//This function should take a number as a parameter and return true if it is odd and false otherwise.
+
+const isOdd = function (num) {
+  return num % 1 === 0;
+}
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
