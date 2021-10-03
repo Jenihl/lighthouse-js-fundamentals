b3ea440c1af691cdb55d1afc896ee3dd45cf3b11
Jen Ihl <jenniferlindatruong@gmail.com>
2021-10-03 04:24:59 +0000

    Switch and array index

---
+++ mayor-codeville.js
@@ -0,0 +1,24 @@
+const moves = ["north", "north", "west", "west", "north", "east", "north"];
+
+const finalPosition = function (moves) {
+  let coor = [0, 0];
+
+  for (let move in moves) {
+    switch(moves[move]) {
+      case 'north':
+        coor[1]++;
+        break;
+        case 'south':
+          coor[1]--;
+          break;
+          case 'west':
+            coor[0]--;
+            break;
+            case 'east':
+              coor[0]++;
+              break;
+    }
+  }
+  return coor;
+}
+console.log(finalPosition(moves))
