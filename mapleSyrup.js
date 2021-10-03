a45e421f296a3a7a137fd4f7f9b96beef3e3c472
Jen Ihl <jenniferlindatruong@gmail.com>
2021-10-03 17:52:36 +0000

    barrels of 100 with modulo

---
+++ mapleSyrup.js
@@ -0,0 +1,11 @@
+function howManyHundreds(num) {
+  return num / 100;
+  return num % 10;
+}
+
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
