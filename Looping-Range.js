7b2d90646906e8292e89e5e2fbc31d52fa1b3f2f
Jen Ihl <jenniferlindatruong@gmail.com>
2021-10-04 02:56:15 +0000

    arry of numbers start to end counting step with parameters

--- Looping-Range.js
+++ Looping-Range.js
@@ -1,16 +1,9 @@
-const range = function (start, end, step){
-let walking = [];
-for (i = start; i <= end; i += step){
-  walking.push(i);
-  if (start > end){
-  console.log(walking)
-} else if (step <= 0){
-  console.log(walking)
-}
-};
-return walking
+const range = function(start, end, step) {
+  const result = []
+  for (let i = start; i <= end; i += step) {
+    result.push(i);
+  }
+  return result;
 }
 
 console.log(range(0, 10, 2));
-console.log(range(10, 30, 5));
-console.log(range(-5, 2, 3));
